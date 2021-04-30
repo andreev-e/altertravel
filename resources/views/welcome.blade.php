60c0a0a753837d6be16fad22186c70bc444f6d2e
Evgeniy Andreev <andreev-e@mail.ru>
2021-04-30 20:49:10 +0300

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="css/app.css" type="text/css">
+        <script src="js/app.js"></script>
+
+    </head>
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+<h1>Главная</h1>
+        </div>
+        <div class="col-6 d-none">
+<h2>JQuery работает</h2>
+<script type="text/javascript">
+  alert($('h2').text());
+</script>
+        </div>
+      </div>
+    </div>
+
+
+    </body>
+</html>
