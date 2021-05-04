73f6268eab518acda6c43690d3a05099df852014
Evgeniy Andreev <andreev-e@mail.ru>
2021-05-04 17:30:11 +0300

    Filesystem fix

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,7 +32,7 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => env('APP_URL'),
+            'root' => storage_path('app'),
         ],
 
         'public' => [
