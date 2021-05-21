3166d3de3d22c426ceae63a2dc390afb99144297
Evgeniy Andreev <andreev-e@mail.ru>
2021-05-21 09:45:57 +0300

    Title fix

--- resources/views/tag.blade.php
+++ resources/views/tag.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
-@section('title')Публикации по тегу @endsection
+@section('title')Интересные {{mb_strtolower($tag->name_rod)}} на карте Альтернативного путеводителя@endsection
 @section('content')
 <div class="container">
   <h1>Публикации по тегу &laquo;{{$tag->name}}&raquo;</h1>
   <div class="row">
   @foreach ($pois as $poi)
-    <div class="col-sm-4"><a href="{{ route('single-poi', $poi->url) }}">{{ $poi->name }}</a></div>
+    <div class="col-sm-4"><a href="{{ route('single-poi', $poi->url) }}">{!! $poi->name !!}</a></div>
 @endforeach
 </div>
 </div>
