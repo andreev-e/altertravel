c43a9b44650a8f246c11c363d4ab2050f2992669
Evgeniy Andreev <andreev-e@mail.ru>
2021-05-31 15:54:45 +0300

    Metrika

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -133,5 +133,21 @@
         <main class="py-4">
             @yield('content')
         </main>
+        
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript" >
+           (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+           m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+           (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+           ym(10896850, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true
+           });
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/10896850" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
+
 </body>
 </html>
