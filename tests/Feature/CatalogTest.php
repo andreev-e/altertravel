403d4c9e873fca36617951e27c03f2acdc9dd8b5
Evgeniy Andreev <andreev-e@mail.ru>
2021-06-03 15:04:39 +0300

    Testing

---
+++ tests/Feature/CatalogTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+
+class CatalogTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_locations()
+    {
+        $response = $this->get('/locations');
+        $response->assertStatus(200);
+    }
+
+    public function test_location()
+    {
+        $response = $this->get('/locations/rossiya');
+        $response->assertStatus(200);
+    }
+
+    public function test_tag()
+    {
+        $response = $this->get('/tag/avtomobil');
+        $response->assertStatus(200);
+    }
+
+    public function test_category()
+    {
+        $response = $this->get('/category/arxitektura');
+        $response->assertStatus(200);
+    }
+
+    public function test_tag_location()
+    {
+        $response = $this->get('/tag/avtomobil/azerbaidzan');
+        $response->assertStatus(200);
+    }
+
+    public function test_category_location()
+    {
+        $response = $this->get('/category/arxitektura/armeniya');
+        $response->assertStatus(200);
+    }
+
+}
