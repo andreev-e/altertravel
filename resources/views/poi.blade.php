4a4c2290d2235964eabd83bd04703ba1f88b2f73
Evgeniy Andreev <andreev-e@mail.ru>
2021-06-10 15:09:45 +0300

    Fix

--- resources/views/poi.blade.php
+++ resources/views/poi.blade.php
@@ -92,13 +92,13 @@ $(document).ready(function(){
     nav:true,
     responsive:{
         0:{
-            items:1
+            items:2
         },
         600:{
-            items:1
+            items:3
         },
         1000:{
-            items:1
+            items:4
         }
     }
 })
