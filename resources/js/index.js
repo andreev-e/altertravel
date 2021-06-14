c67f6bbaf76774d2e03d2ff3f95dfe6534703346
Evgeniy Andreev <andreev-e@mail.ru>
2021-06-14 10:38:02 +0300

    fcripts

--- resources/js/index.js
+++ resources/js/index.js
@@ -20,26 +20,6 @@ $(document).ready(function() {
   }
 
 
-  $('.owl-carousel').owlCarousel({
-    loop:false,
-    margin:10,
-    nav:true,
-    items:4,
-    autoWidth:true,
-    autoHeight: false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:4
-        }
-    }
-})
-
 
 });
 
